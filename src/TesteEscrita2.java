5d557ae2404566934fa54dedb8e34530104c68a7
Luiz Felipe <lffm1994@gmail.com>
2023-01-23 00:01:51 -0300

    Saída com FileWriter

---
+++ src/TesteEscrita2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class TesteEscrita2 {
+    public static void main(String[] args) throws IOException {
+        // Fluxo de Escrita de Arquivo
+//        OutputStream fos = new FileOutputStream( "lorem2.txt");
+//        Writer ows = new OutputStreamWriter(fos); // transformar bit e bites em caracters
+//        BufferedWriter bw = new BufferedWriter(ows);
+
+        FileWriter fw = new FileWriter("lorem2.txt");
+        fw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+        fw.write(System.lineSeparator());
+        fw.write(System.lineSeparator());
+        fw.write(System.lineSeparator());
+        fw.write("tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,");
+
+        fw.close();
+    }
+}
